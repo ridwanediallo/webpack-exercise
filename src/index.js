4b126a1b7389b1a11f25042c6f5303bea39635e3
Ridwan <ridwanediallo99@gmail.com>
2022-02-15 15:20:02 +0000

    Add Final setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import _ from "lodash";
-import printMe from "./print.js";
+import "./style.css";
+import Icon from "./image.jpg";
+import Data from "./data.xml";
+import Notes from "./data.csv";
 
 function component() {
   const element = document.createElement("div");
@@ -8,10 +11,16 @@ function component() {
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(["Hello", "webpack"], " ");
 
-  btn.innerHTML = "Click me and check the console!";
-  btn.onclick = printMe;
+  element.classList.add("hello");
 
-  element.appendChild(btn);
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+
+  console.log(Data);
+  console.log(Notes);
 
   return element;
 }
